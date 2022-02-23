459a936f374bc016c64dea61ee223558ab044f9c
nayyyhaa <nehaguptang.ng@gmail.com>
2022-02-23 15:33:05 +0530

    add: login & rules page

---
+++ scripts/login.js
@@ -0,0 +1,15 @@
+/* ---------LOGIN------------*/
+
+const validateForm = document.querySelectorAll(".validate-form");
+
+validateForm.forEach((form) => {
+  form.addEventListener(
+    "submit",
+    (e) => {
+      if (!form.checkValidity()) e.preventDefault();
+      e.preventDefault();
+      form.classList.add("form-validated");
+    },
+    false
+  );
+});
